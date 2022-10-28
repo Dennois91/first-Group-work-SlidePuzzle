d416c12493f1b3ec6f6f9de40788d1b93164bd68
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-10-28 15:21:07 +0200

    Added bList method to GIU

--- src/GameGui.java
+++ src/GameGui.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class GameGui extends JFrame {
 
-
     //Panel layouts
     JPanel northPanel = new JPanel();
     JPanel centerPanel = new JPanel();
@@ -28,7 +27,7 @@ public class GameGui extends JFrame {
     JButton nr14 = new JButton("14");
     JButton nr15 = new JButton("15");
     JButton blank = new JButton(" ");
-    List<JButton> buttonList = new ArrayList<>();
+    List<JButton> buttonList;
 
 
     //Manipulate game buttons
@@ -52,26 +51,12 @@ public class GameGui extends JFrame {
 
         centerPanel.setLayout(new GridLayout(4, 4));
 
-        buttonList.add(nr1);
-        buttonList.add(nr2);
-        buttonList.add(nr3);
-        buttonList.add(nr4);
-        buttonList.add(nr5);
-        buttonList.add(nr6);
-        buttonList.add(nr7);
-        buttonList.add(nr8);
-        buttonList.add(nr9);
-        buttonList.add(nr10);
-        buttonList.add(nr11);
-        buttonList.add(nr12);
-        buttonList.add(nr13);
-        buttonList.add(nr14);
-        buttonList.add(nr15);
-        buttonList.add(blank);
+        buttonList = bList();
 
         for (JButton button : buttonList) {
             centerPanel.add(button);
-            button.addActionListener((i) -> engine.moveButtonValue(i));
+            int temp =1;
+           // button.addActionListener((i) -> engine.moveButtonValue(temp));
         }
 
         southPanel.add(newGame);
@@ -82,5 +67,25 @@ public class GameGui extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    protected List<JButton> bList(){
+        List<JButton> buttons = new ArrayList<>();
+        buttons.add(nr1);
+        buttons.add(nr2);
+        buttons.add(nr3);
+        buttons.add(nr4);
+        buttons.add(nr5);
+        buttons.add(nr6);
+        buttons.add(nr7);
+        buttons.add(nr8);
+        buttons.add(nr9);
+        buttons.add(nr10);
+        buttons.add(nr11);
+        buttons.add(nr12);
+        buttons.add(nr13);
+        buttons.add(nr14);
+        buttons.add(nr15);
+        buttons.add(blank);
+        return buttons;
     }
 }

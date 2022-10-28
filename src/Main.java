e037428bdf1181bcb74ab31b2c2eeea7a6384fed
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-10-28 15:21:07 +0200

    Added bList method to GIU

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
         new Main().run();
     }
+
     private void run() {
+
+      //  Engine engine = Engine.create();
         GameGui gui = new GameGui();
         // Do stuff
     }

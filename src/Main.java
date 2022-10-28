4fee660e9142ee6f917014fc39ee7ffb88689f70
John Norén <super.happy.email.fun@gmail.com>
2022-10-28 10:38:30 +0200

    Test2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
 
-    public Main() {
-        System.out.println("Hello world!");
-
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-
-
+        new Main().run();
+    }
+    private void run() {
+        // Do stuff
     }
 }

6d4c8fb9786ac1f170dc4e667a1e03c84aaa7c29
John Norén <super.happy.email.fun@gmail.com>
2022-10-28 22:43:35 +0200

    Added methods in Engine and cleaned it up

--- src/Engine.java
+++ src/Engine.java
@@ -1,56 +1,35 @@
-import javax.swing.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Engine {
-	private Hashtable<Integer, Integer> buttonValues;
+	private Square emptySquare;
 
-	private Engine(Hashtable<Integer, Integer> buttonValues, List<JButton> buttons) {
-		this.buttonValues = buttonValues;
+	public Engine(Square emptySquare) {
+		this.emptySquare = emptySquare;
 	}
 
-	public static Engine create(List<JButton> buttons) {
-		Hashtable<Integer, Integer> initialButtonValues = new Hashtable<>();
-		IntStream.range(1, 17).forEach((i -> initialButtonValues.put(i, i)));
-		return new Engine(initialButtonValues, buttons);
+	public void move(Square square) {
+		if (canMove(square)) {
+			switchSquareTexts(square, emptySquare);
+			emptySquare = square;
+		}
 	}
 
-	public void moveButtonValue(JButtonPlus buttonId) {
-		int id = buttonId.getIndex();
-		// Check if button is next to 16
+	private void switchSquareTexts(Square a, Square b) {
+		var tempTextHolder = a.getText();
+		a.setText(b.getText());
+		b.setText(tempTextHolder);
 	}
 
-	private boolean isMoveable(int buttonId) {
-		Hashtable<Integer, Set<Integer>> validMoves = new Hashtable<>();
-		validMoves.put(1, Set.of(2, 5));
-		validMoves.put(2, Set.of(1, 3, 6));
-		validMoves.put(3, Set.of(2, 4, 7));
-		validMoves.put(4, Set.of(3, 8));
-		validMoves.put(5, Set.of(1, 6, 9));
-		validMoves.put(6, Set.of(2, 5, 7, 10));
-		validMoves.put(7, Set.of(3, 6, 8, 11));
-		validMoves.put(8, Set.of(4, 7, 12));
-		validMoves.put(9, Set.of(5, 10, 13));
-		validMoves.put(10, Set.of(6, 9, 11, 14));
-		validMoves.put(11, Set.of(7, 10, 12, 15));
-		validMoves.put(12, Set.of(8, 11, 16));
-		validMoves.put(13, Set.of(9, 14));
-		validMoves.put(14, Set.of(10, 13, 15));
-		validMoves.put(15, Set.of(11, 14, 16));
-		validMoves.put(16, Set.of(12, 15));
-		return validMoves.get(getPositionForValue(16)).contains(buttonId);
-	}
-
-
-	private boolean canMove(Square square, Position positionOfEmptyValue) {
-		var possibleMoves = getPossibleMoves(positionOfEmptyValue);
+	private boolean canMove(Square square) {
+		var possibleMoves = getPossibleMoves();
 		return possibleMoves.containsKey(square.getCurrentPosition());
 	}
 
-	private HashMap<Position, Position> getPossibleMoves(Position positionOfEmptyValue) {
-		var adjacentPositions = getAdjacentPositions(positionOfEmptyValue);
+	private HashMap<Position, Position> getPossibleMoves() {
+		var adjacentPositions = getAdjacentPositions(emptySquare.getCurrentPosition());
 		var possibleMoves = new HashMap<Position, Position>();
-		adjacentPositions.forEach((adjacentPosition -> possibleMoves.put(adjacentPosition, positionOfEmptyValue)));
+		adjacentPositions.forEach((adjacentPosition -> possibleMoves.put(adjacentPosition,
+				emptySquare.getCurrentPosition())));
 		return possibleMoves;
 	}
 

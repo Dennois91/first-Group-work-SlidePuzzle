18203fe60548559ff7153c48ec6dcf3acd412350
John Norén <super.happy.email.fun@gmail.com>
2022-10-28 21:59:06 +0200

    Added Position equals and hashcode

--- src/Position.java
+++ src/Position.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Position {
 	private final int x;
 	private final int y;
@@ -22,5 +24,18 @@ public class Position {
 	}
 	public Position right() {
 		return new Position(this.x+1,this.y);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {return true;}
+		if (o == null || getClass() != o.getClass()) {return false;}
+		Position position = (Position) o;
+		return x == position.x && y == position.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
 	}
 }

d1ee80e304ca44f725c14a2182e9a5e06beb0e48
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-10-31 22:36:37 +0100

    Game Complete. is Solvable incomplete

--- src/models/ProgressChecker.java
+++ src/models/ProgressChecker.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProgressChecker {
@@ -21,5 +23,36 @@ public class ProgressChecker {
                 .map(grid::get)
                 .allMatch(square -> Integer.parseInt(square.getText().equals("")
                         ? "0" : square.getText()) == counter.getAndIncrement());
+    }
+
+
+
+
+
+
+
+    // test solver
+
+    public boolean isSolvable(int gameSize) {
+        List<String> values = new ArrayList<>();
+        grid.forAll(square -> values.add(square.getText()));
+        System.out.println(values);
+        List<Integer> intValues = new ArrayList<>();
+        for (String s : values) {
+            if (!s.isBlank())
+                intValues.add(Integer.parseInt(s));
+        }
+        System.out.println(intValues);
+        int inv_count = 0;
+        for (int i = 0; i < gameSize - 1; i++)
+            for (int j = i + 1; j < gameSize; j++)
+                if (intValues.get(i) > intValues.get(j))
+                    inv_count++;
+        System.out.println(inv_count);
+        values.clear();
+        intValues.clear();
+        return inv_count % 2 == 0;
+
+
     }
 }

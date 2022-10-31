48aeb39af39bc414db1bc0714b1abe6a3ce0e03b
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-10-31 18:54:16 +0100

    Game Complete. Difficulty modefier added

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,41 +4,59 @@ import main.Game;
 import models.*;
 import view.View;
 
+import java.awt.event.ActionEvent;
+import java.util.Objects;
+
 public class Controller {
-	private final ProgressChecker progressChecker;
-	private final View view;
-	private final Mover mover;
+    private final ProgressChecker progressChecker;
+    private final View view;
+    private final Mover mover;
+    private String difficulty = "normal";
 
-	public Controller(View view, ProgressChecker progressChecker, Mover mover) {
-		this.view = view;
-		this.mover = mover;
-		this.progressChecker = progressChecker;
-	}
+    public Controller(View view, ProgressChecker progressChecker, Mover mover) {
+        this.view = view;
+        this.mover = mover;
+        this.progressChecker = progressChecker;
+    }
 
-	public void initView() {
 
-	}
+    public void initializeController() {
+        view.getSquareGrid().forAll(square -> square.addActionListener(e -> squareClicked(square)));
+        view.getCheatButton().addActionListener(e -> cheatButtonClicked());
+        view.getNewGameButton().addActionListener(e -> NewGameButtonClicked());
+        view.getEasy().addActionListener(this::Difficulty);
+        view.getNormal().addActionListener(this::Difficulty);
+        view.getHard().addActionListener(this::Difficulty);
 
-	public void initializeController() {
-		view.getSquareGrid().forAll(square -> square.addActionListener(e -> squareClicked(square)));
-		view.getCheatButton().addActionListener(e -> cheatButtonClicked());
-	    view.getNewGameButton().addActionListener(e -> NewGameButtonClicked());
-	}
+    }
 
-	private void cheatButtonClicked() {
-		Game.cheat().run();
-	}
+    private void cheatButtonClicked() {
+        view.dispose();
+        Game.cheat().run();
+    }
 
-	private void NewGameButtonClicked() {
-		view.dispose();
-		Game.normal().run();
-		// TODO Create a new game
-	}
+    private void NewGameButtonClicked() {
+        view.dispose();
+        if (Objects.equals(difficulty, "easy")) {
+            Game.easy().run();
+        }
+        if (Objects.equals(difficulty, "normal")) {
+            Game.normal().run();
+        }
+        if (Objects.equals(difficulty, "hard")) {
+            Game.hard().run();
+        }
+    }
 
-	public void squareClicked(Square square) {
-		mover.moveSquare(square);
-		progressChecker.checkProgress();
-		view.updatePanel();
+    private void Difficulty(ActionEvent e) {
+        difficulty = (e.getActionCommand());
+    }
 
-	}
+
+    public void squareClicked(Square square) {
+        mover.moveSquare(square);
+        progressChecker.checkProgress();
+        view.updatePanel();
+
+    }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -6,9 +6,11 @@ import models.Square;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class View extends JFrame {
 
     Grid<Square> squareGrid;
+
 
 
     //Panel layouts
@@ -22,10 +24,15 @@ public class View extends JFrame {
     //Manipulate game buttons
     JButton newGame = new JButton("New game");
     JButton cheatGame = new JButton("Ima cheater!");
+    JRadioButton easy = new JRadioButton("Easy");
+    JRadioButton normal = new JRadioButton("Normal");
+    JRadioButton hard = new JRadioButton("Hard");
+    ButtonGroup levels = new ButtonGroup();
 
     //main.Game labels
     JLabel movesCounter = new JLabel(String.valueOf(0));
     JLabel messageLabel = new JLabel("Nr of moves: ");
+
 
     public View(Grid<Square> squareGrid) {
 
@@ -41,15 +48,24 @@ public class View extends JFrame {
         northPanel.setBackground(Color.YELLOW);
 
         centerPanel.setLayout(new GridLayout(squareGrid.getRow(), squareGrid.getCol()));
-        System.out.println(squareGrid.getRow());
-        System.out.println(squareGrid.getCol());
 
         squareGrid.forAll(square -> centerPanel.add(square));
 
+        levels.add(easy);
+        levels.add(normal);
+        levels.add(hard);
 
+        easy.setActionCommand("easy");
+        normal.setActionCommand("normal");
+        hard.setActionCommand("hard");
 
         southPanel.add(newGame);
+        southPanel.add(easy);
+        southPanel.add(normal);
+        southPanel.add(hard);
+
         southPanel.add(cheatGame);
+
         southPanel.setBackground(Color.YELLOW);
 
         pack();
@@ -65,6 +81,28 @@ public class View extends JFrame {
         return newGame;
     }
 
+    public JRadioButton getEasy() {
+        return easy;
+    }
+    public JRadioButton getNormal(){
+        return normal;
+    }
+    public JRadioButton getHard(){
+        return hard;
+    }
+
+    public void setEasy(JRadioButton easy) {
+        this.easy = easy;
+    }
+
+    public void setNormal(JRadioButton normal) {
+        this.normal = normal;
+    }
+
+    public void setHard(JRadioButton hard) {
+        this.hard = hard;
+    }
+
     public void updatePanel (){
      centerPanel.removeAll();
      squareGrid.forAll(square -> centerPanel.add(square));
@@ -75,4 +113,6 @@ public class View extends JFrame {
     public Grid<Square> getSquareGrid() {
         return squareGrid;
     }
+
+
 }

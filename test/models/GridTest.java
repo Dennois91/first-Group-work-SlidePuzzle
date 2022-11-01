3f4986f287c3e80d20a33e265930034211c491e3
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2022-11-01 12:33:48 +0100

    Game Complete. Added test for swap method in GridTest

--- test/models/GridTest.java
+++ test/models/GridTest.java
@@ -1,8 +1,19 @@
 package models;
 
+
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class GridTest {
+
+    Grid<Square> grid = new Grid<>(new Square[2][2]);
+    Position a = new Position(1, 1);
+    Position b = new Position(1, 2);
+    Position c = new Position(2, 1);
+    Position d = new Position(2, 2);
 
     @Test
     void findPosition() {
@@ -10,6 +21,17 @@ class GridTest {
 
     @Test
     void swap() {
+        grid.set(a, new Square("1"));
+        grid.set(b, new Square("2"));
+        grid.set(c, new Square("3"));
+        grid.set(d, new Square("4"));
+        grid.swap(a, d);
+        List<String> list = new ArrayList<>();
+        grid.forAll(square -> list.add(square.getText()));
+
+        assert list.get(3).equals("1");
+        assert !list.get(0).equals("1");
+
     }
 
     @Test
